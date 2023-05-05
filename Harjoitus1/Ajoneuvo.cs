af6e4414e57a4829eeee3c2a373b2dbead9f5a46
Rajan201822 <rajan.muhammad@student.careeria.fi>
2023-05-05 11:56:07 +0300

    Harjoitus 1 tehty || Ajoneuvo

--- Harjoitus1/Ajoneuvo.cs
+++ Harjoitus1/Ajoneuvo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,25 @@ namespace Harjoitus1
 {
     internal class Ajoneuvo
     {
+        public string Nimi { get; set; }
+
+        public int Nopeus { get; set; } 
+
+        public int Renkaat { get; set; } 
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Ajoneuvo: ");
+            Console.WriteLine("- - Nimi: " + Nimi);
+            Console.WriteLine("- - Nopeus: " + Nopeus);
+            Console.WriteLine("- - Renkaat: " + Renkaat);
+        }
+
+        public String ToString()
+        {
+            string merkkijono = "Ajoneuvo Nimi: " + Nimi + ". Nopeus: " + Nopeus + " km/h Renkaat: " + Renkaat + "kpl "; 
+            
+            return merkkijono; 
+        }
     }
 }
--- Harjoitus1/Program.cs
+++ Harjoitus1/Program.cs
@@ -1,10 +1,36 @@
 ﻿//Harjoitus1
 
+using Harjoitus1;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
+      Ajoneuvo auto = new Ajoneuvo();
+        //asetetaan Oliolle "auto" arvot
+        auto.Nimi = "Toyota";
+        auto.Nopeus = 160;
+        auto.Renkaat = 4;
+
+        //käytetään "auto" olion toimintoja 
+        auto.TulostaData(); 
       
+        //Noudetaan auton tiedot ToString metodilla()
+        string autonTiedot = auto.ToString();
+        Console.WriteLine(autonTiedot);
+
+        //Tyhjä rivi konsoliin 
+        Console.WriteLine(); 
+
+        //luodaan toinen olio luokasta ajoneuvo 
+
+        Ajoneuvo mopo = new Ajoneuvo();
+        mopo.Nimi = "Skootteri";
+        mopo.Nopeus = 55; 
+        mopo.Renkaat = 2;
+
+        mopo.TulostaData(); 
+        Console.Write(mopo.ToString()); 
 
     }
 
