041156eb9fe6442480bf95caa566da55db44a1dc
Rajan201822 <rajan.muhammad@student.careeria.fi>
2023-05-05 12:47:11 +0300

    Harjoitus 2 tehty || Opiskelija

---
+++ Harjoitus2/Opiskelija.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus2
+{
+    internal class Opiskelija
+    {
+        private string Nimi { get; set; }
+
+        private string OpiskelijaID { get; set; }
+
+        private int Opintopisteet { get; set; }
+
+        //konstruktori opsikelija -luokalle 
+        public Opiskelija(string _nimi, string _id, int _op)
+        {
+            Nimi = _nimi;
+            OpiskelijaID = _id;
+            Opintopisteet = _op;
+
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Opiskelija: ");
+            Console.WriteLine("- - Nimi: " + Nimi);
+            Console.WriteLine("- - Opiskelijan ID " + OpiskelijaID);
+            Console.WriteLine("- - Opintopisteet: " + Opintopisteet);
+            Console.WriteLine("");
+        }
+
+        //metodi, joka ottaa parametriksi integer - arvon.
+        public void Muokkaaopintopisteitä(int i)
+        {
+            Opintopisteet += i; 
+
+            if (Opintopisteet < 0)
+            {
+                Opintopisteet = 0; 
+            }
+
+
+
+        }
+        
+    }
+}
--- Harjoitus2/Program.cs
+++ Harjoitus2/Program.cs
@@ -1,8 +1,16 @@
 ﻿//harjoitus2
+using Harjoitus2;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //luodaan opiskelija olio 
+        Opiskelija opiskelija1 = new Opiskelija("Matti", "TVT1234", 0);
+
+        opiskelija1.TulostaData();
+        opiskelija1.Muokkaaopintopisteitä(5);
+        opiskelija1.TulostaData();
+            
     }
 }
