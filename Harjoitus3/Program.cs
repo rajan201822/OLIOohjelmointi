b7734f46d42de9392c6c102b1dbf3885b2c45ad4
Rajan201822 <rajan.muhammad@student.careeria.fi>
2023-05-05 14:07:57 +0300

    kiuas KT tehty

---
+++ Harjoitus3/Program.cs
@@ -0,0 +1,26 @@
+﻿//harjoitus 3 (kT)
+
+using Harjoitus3KT;
+
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+        kiuas harvi1 = new kiuas("Harvia", 0, 0, false);
+        harvi1.TulostaData();
+        harvi1.päälle();
+        harvi1.TulostaData();
+
+        harvi1.Lämpötilaylös();
+        harvi1.TulostaData();
+        harvi1.kosteus();
+        harvi1.TulostaData();
+        
+
+
+
+       
+
+    }
+}
---
+++ Harjoitus3/kiuas.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus3KT
+{
+    internal class kiuas
+    {
+        public string Nimi;
+
+        public int Lämpotila;
+
+        public int Kosteus;
+
+        public bool Päällä;
+
+        public kiuas(string _nimi, int _lämpötila, int _kosteus, bool _päällä)
+        {
+            Nimi = _nimi;
+            Lämpotila = _lämpötila;
+            Kosteus = _kosteus;
+            Päällä = _päällä;
+            
+          
+        }
+
+        public void päälle()
+        {
+            if (Päällä)
+            {
+                Päällä = false;
+            }
+
+            else
+            {
+                Päällä = true; 
+            }
+        }
+        public void Lämpötilaylös()
+        {
+            Console.WriteLine("syötä lämpötila");
+            Lämpotila=int.Parse(Console.ReadLine());
+        }
+
+        public void kosteus()
+        {
+            Console.WriteLine("syötä kiukkaan kosteus");
+            Kosteus = int.Parse(Console.ReadLine());
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("kiuas: ");
+            Console.WriteLine("- - Nimi: " + Nimi);
+            if (Päällä)
+            {
+                Console.WriteLine("kiuas on päällä") ; 
+
+            }
+            else
+            {
+                Console.WriteLine("kiuas ei ole päällä");
+            }
+            Console.WriteLine("- - Lämpötila " + Lämpotila);
+            Console.WriteLine("- - kosteus " + Kosteus);
+            Console.WriteLine("- - päällä " );
+            Console.WriteLine("");
+        }
+    }
+}
